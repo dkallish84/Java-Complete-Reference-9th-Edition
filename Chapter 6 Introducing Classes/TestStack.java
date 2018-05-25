830ca161cf02865ea0cca2b07a0eba1765a7f4f1
dkallish84 <dkallish84@gmail.com>
2018-05-25 14:11:52 -0700

    Additional Mac uploads

---
+++ Chapter 6 Introducing Classes/TestStack.java
@@ -0,0 +1,49 @@
+// This class defines an integer stack that can hold 10 values
+class Stack {
+	int stack[] = new int[10];
+	int tos;
+
+	// Initialize top-of-stack
+	Stack() {
+		tos = -1;
+	}
+
+	// Push an item onto the stack
+	void push(int item) {
+		if(tos == 9) {
+			System.out.println("Stack is full.");
+		} else {
+			stack[++tos] = item;
+		}
+	}
+
+	// Pop an item from the stack
+	int pop() {
+		if(tos < 0) {
+			System.out.println("Stack is empty.");
+			return 0;
+		} else {
+			return stack[tos--];
+		}
+	}
+}
+
+class TestStack {
+	public static void main(String args[]) {
+		Stack mystack1 = new Stack();
+		Stack mystack2 = new Stack();
+
+		// push some numbers into the stack
+		for(int i=0; i<10; i++) mystack1.push(i);
+		for(int i=10; i<20; i++) mystack2.push(i);
+
+		// pop those numbes off the stack
+		System.out.println("Stack in mystack1:");
+		for(int i=0; i<10; i++)
+			System.out.println(mystack1.pop());
+
+		System.out.println("Stack in mystack2:");
+		for(int i=0; i<10; i++)
+			System.out.println(mystack2.pop());
+	}
+}
