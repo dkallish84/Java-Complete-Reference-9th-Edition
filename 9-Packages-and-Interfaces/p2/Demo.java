83fc38cf4d9c6faee6ef0a20928df58676f236e5
dkallish84 <dkallish84@gmail.com>
2018-06-08 20:36:14 -0700

    Demo package p2

---
+++ 9-Packages-and-Interfaces/p2/Demo.java
@@ -0,0 +1,10 @@
+// Demo package p2.
+package p2;
+
+// Instantiate the various classes in p2.
+class Demo {
+	public static void main(String args[]) {
+		Protection2 ob1 = new Protection2();
+		OtherPackage ob2 = new OtherPackage();
+	}
+}
